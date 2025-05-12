2e6617cc339e46cafde6664a24ce80584a53c22b
nianhuawong <nianhuawong@qq.com>
2025-05-12 17:01:57 +0800

    对于单个case，训练和验证如果在较少的样本上，可达到较好的效果

--- neural/GNN_ALM/grid_sample.py
+++ neural/GNN_ALM/grid_sample.py
@@ -94,22 +94,22 @@ def process_single_file(file_path):
     z_min, z_max = (min(zs), max(zs)) if zs else (0.0, 0.0)
 
     # 生成归一化后的坐标列表
-    normalized_nodes = all_nodes
-    # normalized_nodes = []
-    # for node in all_nodes:
-    #     # 处理每个坐标轴
-    #     x_range = x_max - x_min
-    #     norm_x = (node[0] - x_min) / x_range if x_range != 0 else 0.5
+    # normalized_nodes = all_nodes
+    normalized_nodes = []
+    for node in all_nodes:
+        # 处理每个坐标轴
+        x_range = x_max - x_min
+        norm_x = (node[0] - x_min) / x_range if x_range != 0 else 0.5
 
-    #     y_range = y_max - y_min
-    #     norm_y = (node[1] - y_min) / y_range if y_range != 0 else 0.5
+        y_range = y_max - y_min
+        norm_y = (node[1] - y_min) / y_range if y_range != 0 else 0.5
 
-    #     if len(node) > 2 and zs:
-    #         z_range = z_max - z_min
-    #         norm_z = (node[2] - z_min) / z_range if z_range != 0 else 0.5
-    #         normalized_nodes.append((norm_x, norm_y, norm_z))
-    #     else:
-    #         normalized_nodes.append((norm_x, norm_y))
+        if len(node) > 2 and zs:
+            z_range = z_max - z_min
+            norm_z = (node[2] - z_min) / z_range if z_range != 0 else 0.5
+            normalized_nodes.append((norm_x, norm_y, norm_z))
+        else:
+            normalized_nodes.append((norm_x, norm_y))
 
     # 初始化存储结构
     wall_faces = []
