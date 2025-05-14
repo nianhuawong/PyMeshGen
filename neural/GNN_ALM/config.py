7d73794f6bbacf1a928f4343af3a53fe674d3b4c
nianhuawong <nianhuawong@qq.com>
2025-05-14 15:13:01 +0800

    调整训练配置，修改批量大小和学习率调整步长；更新模型保存路径以包含模型名称

--- neural/GNN_ALM/config.py
+++ neural/GNN_ALM/config.py
@@ -9,11 +9,11 @@ MODEL_CONFIG = {
 
 TRAINING_CONFIG = {
     "train_ratio": 1.0,  # 训练集比例
-    "batch_size": 3,  # 批量大小
+    "batch_size": 1,  # 批量大小
     "total_epochs": 50000,  # 总训练轮次
     "log_interval": 50,
     "learning_rate": 0.01,  # 学习率
     "validation_interval": 200,  # 验证间隔
-    "lr_stepsize": 1000,  # 学习率调整步长
+    "lr_stepsize": 2000,  # 学习率调整步长
     "lr_gamma": 0.9,  # 学习率调整因子
 }
